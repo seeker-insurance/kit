afac92bb97c2426d0af59df27093dc39cd29139a
Vinicius Nordi <viniciusnordiesperanca@gmail.com>
2020-10-15 15:05:42 -0300

    Adding way to set pagination information

--- web/pagination/pagination.go
+++ web/pagination/pagination.go
@@ -5,9 +5,6 @@ import (
 	"strconv"
 
 	"net/url"
-
-	"github.com/jinzhu/gorm"
-	"github.com/labstack/echo"
 )
 
 type (
@@ -65,40 +62,8 @@ var MaxPerPage = 100
 // Data pagination data for jsonapi
 var Data Pagination
 
-// Apply apply pagination to a provided scope
-func Apply(c echo.Context, scope *gorm.DB, model interface{}, list interface{}, perPage int) error {
-	page, _ := strconv.Atoi(c.QueryParam("page"))
-	if page == 0 {
-		page = 1
-	}
-
-	var count int
-	if err := scope.Offset(0).Model(model).Count(&count).Error; err != nil {
-		return err
-	}
-
-	strPerPage := c.QueryParam("per_page")
-	if strPerPage != "" {
-		perPage, _ = strconv.Atoi(strPerPage)
-	} else if perPage == 0 {
-		perPage = DefaultPerPage
-	}
-
-	if perPage > MaxPerPage {
-		perPage = MaxPerPage
-	}
-
-	setData(count, perPage, page)
-
-	if err := scope.Offset(offset(page, perPage)).Limit(perPage).
-		Find(list).Error; err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func setData(count int, perPage int, page int) {
+// SetData sets data to show pagination
+func SetData(count int, perPage int, page int) {
 	Data.Count = count
 	pages := float64(count) / float64(perPage)
 	Data.Max = int(math.Ceil(pages))
